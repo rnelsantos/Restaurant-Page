7c97802ac314d4ce07131fa22b2aab06650ba89d
arnel <thesantos395@gmail.com>
2024-06-21 13:10:33 +0800

    done with home template, JS

--- src/index.js
+++ src/index.js
@@ -4,15 +4,27 @@ import logo from '../assets/logo.png'
 
 console.log('Hello');
 
-const logoContainer = document.querySelector("#LogoName")
-  // Add the image to our existing div.
-  const myLogo = new Image();
-  myLogo.src = logo;
 
-  const piscesTitle = document.createElement('h1');
-  piscesTitle.innerText = 'Pisces Coffee Hub';
 
-  myLogo.classList.add('logo');
+//Create logo and title container div
+const main = document.querySelector('main');
+const logoContainer = document.createElement("div");
+logoContainer.classList.add('full-bleed');
+logoContainer.id = "LogoName";
+main.appendChild(logoContainer);
 
-  logoContainer.appendChild(myLogo);
-  logoContainer.appendChild(piscesTitle);
+// Add the image to our existing div.
+const myLogo = new Image();
+myLogo.src = logo;
+myLogo.classList.add('logo');
+logoContainer.appendChild(myLogo);
+
+// Add title text
+const piscesTitle = document.createElement('h1');
+piscesTitle.innerText ='Pisces Coffee Hub';
+piscesTitle.classList.add('titleText');
+logoContainer.appendChild(piscesTitle);
+
+//Highlight current page tab
+const home = document.querySelector('#home');
+home.classList.add('select');
